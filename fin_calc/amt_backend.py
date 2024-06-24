a2dbb74472bdfdbeb56fe3f06662bf3ea5ae3e65
Chris David <christophertorres171@gmail.com>
2024-06-23 19:15:28 -0700

    new feature added

--- fin_calc/amt_backend.py
+++ fin_calc/amt_backend.py
@@ -1,14 +1,17 @@
 import pandas as pd
-import pandas as pd
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 
+
+#Amortization schedule class
 class amortization_schedule:
 
     def __init__(self,loan_amount, loan_term_years, interest_rate,freq='monthly'):
         self.loan_amount = loan_amount
         # Calculate monthly interest rate
-        self.monthly_interest_rate = interest_rate / 12 / 100
+        self.interest = (interest_rate/100) if interest_rate > 1 else interest_rate
+        self.monthly_interest_rate = self.interest / 12
         # Calculate total number of payments
         self.loan_term_years = int(loan_term_years)
         self.total_payments = self.loan_term_years * 12
@@ -82,6 +85,142 @@ class amortization_schedule:
         arc = df[['Remaining Balance','Total Interest Paid','Total Paid']]
         fig = px.line(arc,x=list(range(len(arc))),y = arc.columns).update_layout(xaxis_title='Payment Period')
         return fig
+    
+
+#Annuity schedule class
+class annuity:
+
+    def __init__(self,principal,cont,interest,term):
+        self.principal = principal
+        self.cont = cont
+        self.interest = (interest/100) if interest > 1 else interest
+        self.term = round(term)
+        self.annuity_table = self.generate_annuity_schedule(self.principal,self.cont,
+                                                            self.interest,self.term)
+        self.ending_balance = self.annuity_table.iloc[-1]['Balance']
+        self.total_additions = sum(self.annuity_table['Addition'])-self.principal
+        self.total_interest = round(sum(self.annuity_table['Gain']),2)
+        self.graph = self.generate_annuity_graph(self.annuity_table)
+    #generate annuity table
+    def generate_annuity_schedule(self,principal,cont,interest,term):
+        #initial amount, set as principal plut first contribution
+        amt = principal + cont
+        period = []
+        addition = []
+        gain = []
+        balance = []
+
+        for i in range(term):
+            period.append(i+1)
+            if i > 0:
+                addition.append(cont)
+            else:
+                addition.append(principal+cont)
+            #interest for the current term
+            ti = (amt*interest)
+            var = amt + ti
+            gain.append(round(ti,2))
+            balance.append(round(var,2))
+            amt += (ti+cont)
+        
+        data = pd.DataFrame({'Year':period,'Addition':addition,'Gain':gain,
+                            'Balance':balance})
+        return data
+
+    #generate graph for annuity return
+    def generate_annuity_graph(self,df):
+        data = df[['Addition','Gain','Balance']].copy()
+        #reassing initial addition as inital contribution
+        data.at[0,'Addition']= self.cont
+        #set column as cummulative sum of addition
+        data['Addition'] = data['Addition'].cumsum()
+        #set column as cummulative sum of gain
+        data['Gain'] = data['Gain'].cumsum()
+        fig = px.bar(data,title='Annuity Return')
+        return fig
+
+
+#class to determine return (NPV and IRR) based on individual cash flows
+class investment_ret:
+    
+    def __init__(self,initial,cf,interest):
+        self.cash_flows = [initial] + cf
+        self.interest = (interest/100) if interest > 1 else interest
+        self.npv = self.nominal_npv(self.cash_flows,self.interest) 
+        self.graph = self.generate_graph(self.cash_flows)
+
+    #add cash flow, account for interest
+    def nominal_npv(self,cash_flows,inter):
+        income = cash_flows[0]
+        for i in range(1,len(cash_flows)):
+            alpha = cash_flows[i]/((1+inter)**(i))
+            income += alpha 
+        return income
+    
+    #converge lower and upper bound to obtain present value that equals 0 or is close within tolerance margin
+    def nominal_IRR(self,cash_flows):
+        lower_bound = -1.0
+        upper_bound = 1.0
+        #tolerance (margin of error)
+        tol=1e-6
+        iteration = 0 
+
+        while iteration < 1000:
+            mid_rate = (lower_bound + upper_bound) / 2
+            mid_npv = self.nominal_npv(cash_flows,mid_rate)
+
+            if abs(mid_npv) < tol:
+                return round((mid_rate*100),2)
+            
+            if mid_npv > 0:
+                lower_bound = mid_rate
+            else:
+                upper_bound = mid_rate
+
+            iteration +=1 
+        raise ValueError("IRR did not converge")
+    
+    def generate_graph(self,cash_flows):
+        series = pd.Series(cash_flows)
+        fig = px.line(series,labels={'variable':'return'}, title='Cash Flows History')
+        return fig
+
+#process user input as array of numbers
+def process_input(data):
+    #Aggregate input into valid numerical array
+    if ',' not in data:
+        return 'Please enter data in valid format'
+    alpha = []
+    for x in  data.split(','):  
+        valid = True
+        negative = False
+        cnt = 0
+        var = ''
+        
+        for n in x:
+            if n == '-':
+                negative = True 
+                continue
+            if n == ' ':
+                continue
+            if n.isnumeric() == False and n != '.':
+                valid = False
+            if n == '.':
+                cnt += 1
+    
+            var += n
+            if cnt > 1:
+                valid = False
+                
+            if valid == False:
+                break
+
+        if valid == True:
+            var = float(var)
+            if negative == True:
+                var = -(var)
+            alpha.append(var)
+    return alpha
 
 
 
--- fin_calc/amt_frontend.py
+++ fin_calc/amt_frontend.py
@@ -3,35 +3,87 @@ import pandas as pd
 from amt_backend import *
 
 
-page_icon = ':dollar:'
-st.set_page_config(page_icon=page_icon)
-
-st.title('Amortization Schedule :money_with_wings:')
-
-option = st.selectbox('Select Payment Frequency',('Yearly','Monthly'))
-
-if option == 'Yearly':
-    freq = 'yearly'
-else:
-    freq = 'monthly'
+page_icon = ':money_with_wings:'
+st.set_page_config('Financial Calculator',page_icon=page_icon)
+#user enters financial caculation option
+option = st.sidebar.radio('Menu', ['Amortization','Annuity','NPV and IRR'])
 
 
-principal = st.number_input('Enter Loan Principal:',format="%.2f")
-payment_period = st.number_input('Enter loan term in years:',format="%.2f")
-interest = st.number_input('Enter Interest Rate')
+if option == 'Amortization':
+    #amortization, validate all parameters are entered, select yearly or monthly periods
+    st.title('Amortization Schedule')
 
-if principal and payment_period and interest:
-    amt = amortization_schedule(principal,payment_period,interest,freq)
-    st.write('Montly Payment: $',amt.monthly_payment)
-    st.write('Total paid: $',amt.total_paid)
-    st.write('Total Interest: $',amt.interest_paid)
-    st.plotly_chart(amt.graph)
-    df = amt.schedule
-    st.table(df.style.format({col: '{:.2f}' for col in df.columns if pd.api.types.is_float_dtype(df[col])}))
+    freq_option = st.selectbox('Select Payment Frequency',('Yearly','Monthly'))
 
-else:
-    st.write('Enter Complete Data')
+    if freq_option == 'Yearly':
+        freq = 'yearly'
+    else:
+        freq = 'monthly'
     
+    principal = st.number_input('Enter Loan Principal:',format="%.2f")
+    payment_period = st.number_input('Enter loan term in years:',format="%.2f")
+    interest = st.number_input('Enter Interest Rate')
+    #display amortization metrics, table and graph(line)
+    if principal and payment_period and interest:
+        amt = amortization_schedule(principal,payment_period,interest,freq)
+        st.write('Montly Payment: $',str(amt.monthly_payment))
+        st.write('Total paid: $',str(amt.total_paid))
+        st.write('Total Interest: $',str(amt.interest_paid))
+        st.plotly_chart(amt.graph)
+        df = amt.schedule
+        st.table(df.style.format({col: '{:.2f}' for col in df.columns if pd.api.types.is_float_dtype(df[col])}))
 
-# st.cache_data.clear()
+    else:
+        st.write('Enter Complete Data')
+
+
+
+if option == 'Annuity':
+    st.title('Annuity Schedule')
+    principal = st.number_input('Enter Starting Pirncipal')
+    contribution = st.number_input('Enter Contribution per Period')
+    interest = st.number_input('Enter Interest Rate')
+    per_n = st.number_input('Enter Number of Periods')
+    #display annuity metrics, table and graph(stacked bar chart)
+    if principal and contribution and interest and per_n:
+        annuity_n = annuity(principal,contribution,interest,per_n)
+        st.write("Ending Balance:",str(annuity_n.ending_balance))
+        st.write("Starting Principal:",str(annuity_n.principal))
+        st.write("Total Additions:",str(annuity_n.total_additions))
+        st.write("Total Interest Earned:",str(annuity_n.total_interest))
+        st.plotly_chart(annuity_n.graph)
+        df = annuity_n.annuity_table
+        st.table(df.style.format({col: '{:.2f}' for col in df.columns if pd.api.types.is_float_dtype(df[col])}))
+        
+
+    else:
+        st.write('Enter Complete Data')
+
+elif option == 'NPV and IRR':
+    #user enters initial investment, interest and periodic cash flows
+    st.title('Net Present Value & Internal Rate of Return')
+    initial = st.number_input('Enter Initial investment')
+    if initial > 0:
+        initial = -(initial)
+    cash_flows = st.text_area('Enter subsequent cash flows separated by " , " (comma):')
+    cf = process_input(cash_flows)
+    interest_rate = st.number_input('Enter interest rate:')
+
+    if initial and (type(cf) == list):
+        
+        ret = investment_ret(initial,cf,interest_rate)
+        
+        st.write('Net Present Value:',str(round(ret.npv, 2)))
+        try:
+            
+            st.write(f'Internal Rate or Return: {ret.nominal_IRR(ret.cash_flows)} %')
+        except ValueError as e:
+            st.error(str(e))
+
+        st.plotly_chart(ret.graph)
+    else:
+        st.write('Enter Complete Data')
+
+        
+
 
